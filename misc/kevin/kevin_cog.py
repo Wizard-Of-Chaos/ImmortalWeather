5c2aabedc303c3008f4c981489f5380b1f4cabe4
Alexander Wiecking <sandosius@gmail.com>
2026-08-10 16:49:14 -0400

    Kevin maintenance msg

--- misc/kevin/kevin_cog.py
+++ misc/kevin/kevin_cog.py
@@ -2,6 +2,8 @@ import discord as dc
 from discord.ext import commands
 from discord import app_commands
 import subprocess
+from datetime import datetime
+from pytz import timezone
 
 KEVIN_LOCATION = "misc/kevin/kevin.jpg"
 CURL_CMD = ["curl", "--http0.9", "http://192.168.1.163/kevin", "--output", KEVIN_LOCATION ]
@@ -13,8 +15,10 @@ class KevinCog(commands.Cog):
 
     @app_commands.command(name="kevin", description="Get Kevin's current status.")
     async def kevin(self, interaction: dc.Interaction):
+        
+        await interaction.response.send_message("Kevin is under maintenance! Here's the last Kevin pic for now: ", file=dc.File(KEVIN_LOCATION))
+        return
+
         cb: dc.InteractionCallbackResponse = await interaction.response.defer(ephemeral=False, thinking=True)
-        #int_msg: dc.InteractionMessage = cb.resource
         result = subprocess.run(CURL_CMD)
         await interaction.followup.send(file=dc.File(KEVIN_LOCATION))
-        #await int_msg.edit(file=dc.File(KEVIN_LOCATION))
