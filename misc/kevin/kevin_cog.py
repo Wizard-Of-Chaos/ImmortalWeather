dc0e6f2dc9ec424d754e5b6c2b35e50e68b176a9
Alexander Wiecking <sandosius@gmail.com>
2026-08-22 12:32:06 -0400

    Some status updates while assembly is ongoing

--- misc/kevin/kevin_cog.py
+++ misc/kevin/kevin_cog.py
@@ -5,9 +5,12 @@ from datetime import datetime
 import aiohttp
 import glob
 import os
+from PIL import Image
+
+_KVN_LOCAL_IP:str = "http://192.168.1.163"
 
 KEVIN_LOCATION:str = "misc/kevin/shots/"
-KEVIN_URL:str = "http://192.168.1.163/kevin"
+KEVIN_URL:str = f"{_KVN_LOCAL_IP}/kevin"
 
 class KevinCog(commands.Cog):
     def __init__(self, bot):
@@ -15,19 +18,38 @@ class KevinCog(commands.Cog):
         self.bot = bot
         print("Lime observation ready!")
 
-    async def _most_recent_kevin(self) -> dc.File:
+    def _most_recent_kevin(self) -> dc.File:
         list_of_files = glob.glob(KEVIN_LOCATION)
         latest = max(list_of_files, key=os.path.getctime)
-        return dc.file(latest)
+        return dc.File(latest)
 
     def _kevin_fname(self):
         return f"{KEVIN_LOCATION}kevin-{datetime.now().strftime("%s")}.jpg"
 
+    def _shots_cleanup(self):
+        kevinpic_count = len([name for name in os.listdir(KEVIN_LOCATION) if os.path.isfile(os.path.join(KEVIN_LOCATION, name))])
+        print(f"Kevin pic count: {kevinpic_count}")
+        if kevinpic_count > 20:
+            list_of_files = glob.glob(KEVIN_LOCATION)
+            print("Dropping oldest Kevin")
+            oldest = min(list_of_files, key=os.path.getctime)
+            os.remove(oldest)
+
+    def _flip_image(self, fname):
+        img = Image.open(fname)
+        flipped = img.transpose(method=Image.Transpose.FLIP_TOP_BOTTOM)
+        img.close()
+        final = flipped.transpose(method=Image.Transpose.FLIP_LEFT_RIGHT)
+        flipped.close()
+        final.save(fname)
+        final.close()
+
     @app_commands.command(name="kevin", description="Get Kevin's current status.")
     async def kevin(self, interaction: dc.Interaction):
         
-        # await interaction.response.send_message("Kevin is under maintenance! Here's the last Kevin pic for now: ", file=dc.File(self._kevin_fname()))
-
+        await interaction.response.send_message("Kevin is under maintenance! Here's the last Kevin pic for now: ", file=dc.File(self._most_recent_kevin()))
+        return
+    
         cb: dc.InteractionCallbackResponse = await interaction.response.defer(ephemeral=False, thinking=True)
         try:
             async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=60)) as session:
@@ -47,16 +69,9 @@ class KevinCog(commands.Cog):
                                 break
                             kevin.write(content)
                         kevin.close()
-                        await interaction.followup.send(file=dc.File(filename))
-
-                    #cleanup
-                    kevinpic_count = len([name for name in os.listdir(KEVIN_LOCATION) if os.path.isfile(os.path.join(KEVIN_LOCATION, name))])
-                    print(f"Kevin pic count: {kevinpic_count}")
-                    if kevinpic_count > 20:
-                        list_of_files = glob.glob(KEVIN_LOCATION)
-                        print("Dropping oldest Kevin")
-                        oldest = min(list_of_files, key=os.path.getctime)
-                        os.remove(oldest)
+                    #self._flip_image(filename)
+                    await interaction.followup.send(file=dc.File(filename))
+                    self._shots_cleanup()
 
         except aiohttp.ClientConnectionError:
             await interaction.followup.send("Connection aborted -- the WiFi probably disconnected again. Here's the previous Kevin photo.", file=self._most_recent_kevin())
